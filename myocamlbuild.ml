0d8a146ad1e8f5f98664171df399c5efe3dc8262
Nicholas Lucaroni <nicholas.r.lucaroni@gmail.com>
2013-08-21 14:12:43 -0400

    Moved bitvector C files in own directory; scripted
    - Fixed issues I had with scripting bitvector in their own directory
    - Added build support for vectorization; more is needed, like a configuration
      step to choose the method, and add the files to the libphyloc.clib file --can
      this be done programatically based on state in myocamlbuild.ml?

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,54 +1,87 @@
 open Ocamlbuild_plugin
 open Command
 
+(** Modifiable parameters *)
+let cc      = "gcc"
+let cflags  = ["-O2"; "-Wall"; "-pedantic";"-Wextra"]
+let clibs   = ""
+let mlflags = ["-w"; "@a"]
+let vectorization = None
+
+(** Constants *)
+let bv_width = ["8";"16";"32";"64"]
+
+(** Helper functions *)
 let rec arg_weave p = function
   | x::xs -> (A p) :: (A x) :: arg_weave p xs
   | []    -> []
 
-let cc      = "gcc"
-let cflags  = arg_weave "-ccopt" ["-O2"; "-Wall"; "-pedantic";"-Wextra"]
-let clibs   = ""
-let mlflags = ["-w"; "@a"]
+let ($) a b = a b
+let (|>) a b = b a
 
+(** C-Header files *)
 let headers =
-   let bv_ w = "lib/bv"^w^".h" in
-   let bv = [bv_ "8"; bv_ "16"; bv_ "32"; bv_ "64"] in
-  "lib/seq.h" :: "lib/mlmodel.h" :: "lib/phyloc.h" :: bv
+  let headers = 
+    ["lib/seq.h"; "lib/mlmodel.h"; "lib/phyloc.h"]
+  in
+  let bv_ w =
+    let base = "lib/bitvector/bv"^w^".h" in 
+    match vectorization with
+    | None          -> [base]
+    | Some `SSE4_2
+    | Some `SSE4_1
+    | Some `SSE3_0
+    | Some `SSE2_0  -> ["lib/bitvector/bv"^w^"_sse.h"; base]
+    | Some `AVX     -> ["lib/bitvector/bv"^w^"_avx.h"; base]
+    | Some `Altivec -> ["lib/bitvector/bv"^w^"_alti.h"; base]
+    | Some `Neon    -> ["lib/bitvector/bv"^w^"_neon.h"; base]
+  in
+  List.rev_append headers (List.flatten $ List.map bv_ bv_width)
 
 let () = dispatch begin function
   | After_rules ->
-    let bitvector_rule prefix filename extension oth_ext width options : unit =
-      let deps = match oth_ext with
-        | Some x -> ["lib/"^prefix^width^filename^x]
-        | None   -> []
+    let bitvector_rule filename extension dep_ext width : unit =
+      let dep  = "lib/bitvector/bv"^filename^extension
+      and prod = "lib/bitvector/bv"^width^filename^extension in
+      let deps = match dep_ext with
+        | Some x -> [dep;"lib/bitvector/bv"^width^filename^x]
+        | None   -> [dep]
       in
-      let dep  = "lib/"^prefix^filename^extension
-      and prod = "lib/"^prefix^width^filename^extension in
-      rule prod
-        ~prod ~deps:(dep::deps)
-        begin fun evn _build ->
+      rule ("Generate "^prod) ~prod ~deps
+        begin fun _ _ ->
           Cmd (S [A "sed"; A"-e"; A("s/bv\\.h/bv"^width^".h/g");
                            A"-e"; A("s/bv_/bv"^width^"_/g");
                            Sh ("< "^dep); Sh("> "^prod)])
         end
     in
-    let bv_widths = ["8";"16";"32";"64"] in
-    List.iter (fun w -> bitvector_rule "bv" "" ".c" (Some ".h") w []) bv_widths;
-    List.iter (fun w -> bitvector_rule "bv" "" ".h" None w []) bv_widths;
-    (* List.iter (fun w -> bitvector_rule "bv" "_sse" ".c" w [`SSE]) bv_widths; *)
-    (* List.iter (fun w -> bitvector_rule "bv" "_sse" ".h" w [`SSE]) bv_widths; *)
+    List.iter
+      (fun namext ->
+        List.iter (bitvector_rule namext ".c" (Some ".h")) bv_width;
+        List.iter (bitvector_rule namext ".h" (None)     ) bv_width)
+      [""; "_sse"; "_neon"; "_avx"; "_alti"];
 
-    flag ["c"; "use_bv8"]  (S [A"-ccopt";A"-DWIDTH=8" ]);
-    flag ["c"; "use_bv16"] (S [A"-ccopt";A"-DWIDTH=16"]);
-    flag ["c"; "use_bv32"] (S [A"-ccopt";A"-DWIDTH=32"]);
-    flag ["c"; "use_bv64"] (S [A"-ccopt";A"-DWIDTH=64"]);
+    let bv_cflags width =
+      let cflags = ["-DWIDTH="^width] in
+      let cflags = match vectorization with
+        | None          -> cflags
+        | Some `SSE4_2  -> "-msse4.2"::cflags
+        | Some `SSE4_1  -> "-msse4.1"::cflags
+        | Some `SSE3_0  -> "-msse3"::cflags
+        | Some `SSE2_0  -> "-msse2"::cflags
+        | Some `AVX     -> "-mavx"::cflags
+        | Some `Altivec -> "-maltivec"::cflags
+        | Some `Neon    -> "-funsafe-math-optimizations"::"-mfpu=‘neon’"::cflags
+      in
+      arg_weave "-ccopt" cflags
+    in
+    List.iter (fun w -> flag ["c"; "use_bv"^w] (S (bv_cflags w))) bv_width;
 
     dep ["ocaml";"link"; "use_phyloc"] ["libphyloc.a"];
     dep ["c"; "compile"]  headers;
 
     flag ["ocaml"; "link";"use_phyloc"]   (S [A"-cclib";A"-lphyloc"]);
     flag ["ocaml"; "compile"] (S [A"-cc";A cc]);
-    flag ["ocaml"; "compile"] (S cflags);
+    flag ["ocaml"; "compile"] (S (arg_weave "-ccopt" cflags));
     ()
   | _ -> ()
 end
