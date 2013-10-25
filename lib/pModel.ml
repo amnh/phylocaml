744f9ba2d71013370350b1c459645f8de86eb99b
Nicholas Lucaroni <nicholas.r.lucaroni@gmail.com>
2013-10-25 17:55:15 -0400

    type level enforcement to avoid affine (affine ..) state

--- lib/pModel.ml
+++ lib/pModel.ml
@@ -11,11 +11,15 @@
     would need to be expanded to a full cost-matrix with median assignments to
     be used. This specification allows a transformation to that state with a
     compressed format for data-transfer and IO. *)
+type basic_tcm =
+  [ `Const  of int               (** A single cost between differing states. *)
+  | `Linear of int * int         (** A cost between states is equal, except indel. *)
+  | `TCM    of int array array ] (** General TCM contains a full matrix. *)
+
+(** Type-level enforcement of non affine of affine state for tcm. *)
 type tcm =
-  | Const  of int             (** A single cost between differing states. *)
-  | Linear of int * int       (** A cost between states is equal, except indel. *)
-  | Affine of tcm * int       (** A recursive type for adding affine costs. *)
-  | TCM    of int array array (** General TCM contains a full matrix. *)
+  [ `Affine of basic_tcm * int
+  | basic_tcm ]
 
 (** Define options on defining assignments and other details of the cost-matrix. *)
 type options =
