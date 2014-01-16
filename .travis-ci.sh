6858f9e877d15cbac5d55c846ce5449222f52fe4
Nicholas Lucaroni <nicholas.r.lucaroni@gmail.com>
2014-01-16 10:59:21 -0500

    fix travis build #23

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,18 +1,20 @@
 OPAM_DEPENDS="ocamlfind ocamlfind ounit pareto"
+GSL="gsl-bin libgsl0-dev"
+OCAML="ocaml ocaml-native-compilers camlp4-extra opam"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
   3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
   3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
-  4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
-  4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-  4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
-  4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+  4.00.1,1.0.0) ppa=avsm/ocaml40+opam10  ;;
+  4.00.1,1.1.0) ppa=avsm/ocaml40+opam11  ;;
+  4.01.0,1.0.0) ppa=avsm/ocaml41+opam10  ;;
+  4.01.0,1.1.0) ppa=avsm/ocaml41+opam11  ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install -qq $OCAML $GSL
 export OPAMYES=1
 export OPAMVERBOSE=1
 echo OCaml version
@@ -20,14 +22,14 @@ ocaml -version
 echo OPAM versions
 opam --version
 opam --git-version
-opam init 
+opam init
 
 opam update
 opam install ${OPAM_DEPENDS}
 eval `opam config env`
 
 echo "==== LIBRARY ===="
-make 
+make
 echo "==== INSTALL ===="
 make install
 echo "===== TESTS ====="
