f14a29e86be8023c3258ccb60649ace8a39e0041
Nicholas Lucaroni <nlucaroni@amnh.org>
2014-01-16 11:08:21 -0500

    another fix for travis-ci #23

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -15,7 +15,7 @@ esac
 
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq $OCAML $GSL
+sudo apt-get install -qq $OCAML $GSL $LAPACK
 export OPAMYES=1
 export OPAMVERBOSE=1
 echo OCaml version
