3a21e0ea3207b969c31ff66594e9be10289ba213
Nicholas Lucaroni <nlucaroni@amnh.org>
2014-01-31 22:34:06 -0500

    Update Installation (docs); Travis CI (clean-up); META (add num);

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,36 +1,47 @@
 OPAM_DEPENDS="ocamlfind ocamlfind ounit pareto"
-GSL="gsl-bin libgsl0-dev"
-LAPACK="liblapack-dev libblas-dev gfortran"
-OCAML="ocaml ocaml-native-compilers camlp4-extra opam"
+APT_DEPENDS="gsl-bin libgsl0-dev liblapack-dev libblas-dev gfortran"
+APT_OCAML="ocaml ocaml-native-compilers camlp4-extra opam aspcud"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
-  3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
-  3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
   4.00.1,1.0.0) ppa=avsm/ocaml40+opam10  ;;
   4.00.1,1.1.0) ppa=avsm/ocaml40+opam11  ;;
+  4.00.1,1.1.1) ppa=avsm/ocaml40+opam111 ;;
   4.01.0,1.0.0) ppa=avsm/ocaml41+opam10  ;;
   4.01.0,1.1.0) ppa=avsm/ocaml41+opam11  ;;
+  4.01.0,1.1.1) ppa=avsm/ocaml41+opam111 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
-echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo add-apt-repository -y ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq $OCAML $GSL $LAPACK
-export OPAMYES=1
-export OPAMVERBOSE=1
-echo OCaml version
-ocaml -version
-echo OPAM versions
-opam --version
-opam --git-version
-opam init
+sudo apt-get install -qq $APT_OCAML $APT_DEPENDS
 
-opam update
-opam install ${OPAM_DEPENDS}
+# setup OPAM
+opam -y init
 eval `opam config env`
+opam -y update
+opam -y install ${OPAM_DEPENDS}
 
-echo "==== CONFIGURATION DONE ===="
-
+# make/test application
 make
 make test.native
 ./test.native
+
+# install/test application linking
+make install
+make extests
+./test/test.native
+
+# make documentation
+make phylocaml.html
+
+# run toplevel with phylocaml TODO
+#echo "#use \"topfind\";;
+#      #require \"phylocaml\";;
+#      open Phylocaml.Tree;;" > test.ml
+#ocaml test.ml
+
+#make and execute each app in app/ TODO
+#make apps.all
+#make apps.test.native
+#./apps/test.native
--- test/README.md
+++
@@ -1,6 +0,0 @@
-Phylocaml Testing
-=================
-
-Run **make tests** to generate **./test.native** and execute tests. From this
-directory the tests are run with phylocaml as an external package.
-
