059035526d42707cd75411ec5867ece10b7d670f
Nicholas Lucaroni <nlucaroni@amnh.org>
2014-09-23 00:04:52 -0400

    commit another missing test function

--- test/testInternal.ml
+++ test/testInternal.ml
@@ -3,6 +3,16 @@ include Phylocaml
 END
 include Internal
 
+let random_seed =
+  OUnit2.Conf.make_string_opt "random_seed" None "Random seed applied to each test"
+
+let set_random_seed ctxt = 
+  begin match random_seed ctxt with
+    | None   -> truncate @@ Unix.time ()
+    | Some x -> int_of_string x
+  end
+    |> Random.init
+
 let assert_equal_list elt_printer elt_compare =
   let printer xs = 
     let b = Buffer.create 10 in
@@ -14,6 +24,20 @@ let assert_equal_list elt_printer elt_compare =
     let msg = "List length not equal" and printer = string_of_int in
     OUnit2.assert_equal ~msg ~printer (List.length x) (List.length y);
     List.fold_left2 (fun acc a b -> acc && elt_compare a b) true x y
+  in
+  OUnit2.assert_equal ~printer ~cmp
+
+let assert_equal_array sep elt_printer elt_compare =
+  let printer xs = 
+    let b = Buffer.create 10 in
+    Array.iter (fun x -> Buffer.add_string b sep;
+                         Buffer.add_string b (elt_printer x))
+              xs;
+    Buffer.contents b
+  and cmp x y =
+    let msg = "List length not equal" and printer = string_of_int in
+    OUnit2.assert_equal ~msg ~printer (Array.length x) (Array.length y);
+    array_fold_left2 (fun acc a b -> acc && elt_compare a b) true x y
   in
   OUnit2.assert_equal ~printer ~cmp
 
@@ -36,14 +60,6 @@ let assert_equal_ids =
 let assert_equal_num =
   let printer = Num.string_of_num in
   OUnit2.assert_equal ~printer
-
-let assert_equal_tree =
-  let printer x =
-    let b = Buffer.create 100 in
-    let () = Tree.pp_tree (Format.formatter_of_buffer b) x in
-    Buffer.contents b
-  and cmp a b = 0 = (Tree.compare a b) in
-  OUnit2.assert_equal ~printer ~cmp
 
 let assert_equal_tuple ?(container=("[","]")) ?(sep=",") to_string =
   let printer (a,b) = (fst container) ^ (to_string a) ^ sep ^ (to_string b) ^ (snd container)
